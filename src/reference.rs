278f23f37c5fadbc1fa251f6e3932fe4affe91b1
cyberia <cyberia@donk.systems>
2026-03-26 21:13:17 +0000

    rename lifetime in ref

--- src/reference.rs
+++ src/reference.rs
@@ -3,9 +3,9 @@ use alloc::string::{String, ToString};
 use core::str;
 
 #[derive(Debug)]
-pub struct Ref<'repo, D> {
+pub struct Ref<'r, D> {
     inner: RefType,
-    repo: &'repo Repo<D>,
+    repo: &'r Repo<D>,
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -14,24 +14,24 @@ pub enum RefType {
     Symbolic(String),
 }
 
-impl<'repo, D: Directory> Ref<'repo, D> {
-    pub const fn direct(repo: &'repo Repo<D>, oid: ObjectId) -> Self {
+impl<'r, D: Directory> Ref<'r, D> {
+    pub const fn direct(repo: &'r Repo<D>, oid: ObjectId) -> Self {
         Ref {
             inner: RefType::Direct(oid),
             repo,
         }
     }
 
-    pub const fn symbolic(repo: &'repo Repo<D>, target: String) -> Self {
+    pub const fn symbolic(repo: &'r Repo<D>, target: String) -> Self {
         Ref {
             inner: RefType::Symbolic(target),
             repo,
         }
     }
 
-    pub(crate) fn from_content(repo: &'repo Repo<D>, content: &[u8]) -> GResult<Self, D> {
-        if content.starts_with(b"ref: ") {
-            let target = str::from_utf8(&content[5..])?.trim().to_string();
+    pub(crate) fn from_content(repo: &'r Repo<D>, content: &[u8]) -> GResult<Self, D> {
+        if let Some(target) = content.strip_prefix(b"ref: ") {
+            let target = str::from_utf8(target)?.trim().to_string();
             Ok(Ref::symbolic(repo, target))
         } else {
             let oid = ObjectId::from_encoded(content)?;
