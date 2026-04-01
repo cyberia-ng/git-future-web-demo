8a460bede8a5ea83d8e05fc46a1f7296922b8d6c
cyberia <cyberia@donk.systems>
2026-04-01 01:26:52 +0100

    directory error handling

--- rust/rgit-web/src/directory.rs
+++ rust/rgit-web/src/directory.rs
@@ -1,5 +1,3 @@
-use std::any::Any;
-
 use js_sys::{
     Array, ArrayBuffer, AsyncIterator, Function, Promise, Reflect, TypeError, Uint8Array,
 };
@@ -26,82 +24,64 @@ impl WebDirectory {
     }
 }
 
-fn to_directory_error<T: Any>(value: T) -> DirectoryError {
+fn to_directory_error(value: JsValue) -> DirectoryError {
     DirectoryError(Box::new(value))
 }
 
 impl Directory for WebDirectory {
     async fn open_subdir(&self, name: &[u8]) -> Result<Self, DirectoryError> {
-        let handle = self
-            .handle
-            .get_directory_handle(str::from_utf8(name).map_err(to_directory_error)?)
-            .await
-            .map_err(to_directory_error)?;
-        let handle: FileSystemDirectoryHandle = handle
-            .dyn_into()
-            .map_err(|_| TypeError::new("FileSystemDirectoryHandle"))
-            .map_err(to_directory_error)?;
-        Ok(Self { handle })
+        let f = async || -> Result<Self, JsValue> {
+            let handle = self
+                .handle
+                .get_directory_handle(
+                    str::from_utf8(name).map_err(|_| TypeError::new("name was not UTF-8"))?,
+                )
+                .await?;
+            let handle: FileSystemDirectoryHandle = handle.dyn_into()?;
+            Ok(Self { handle })
+        };
+        f().await.map_err(to_directory_error)
     }
 
     async fn list_dir(&self) -> Result<Vec<Vec<u8>>, DirectoryError> {
-        let keys_fn: Function = Reflect::get(&self.handle, &JsValue::from("keys"))
-            .map_err(|_| TypeError::new("FileSystemDirectoryHandle.keys was missing"))
-            .map_err(to_directory_error)?
-            .dyn_into()
-            .map_err(|_| TypeError::new("FileSystemDirectoryHandle.keys was not a function"))
-            .map_err(to_directory_error)?;
-        let keys_iter: AsyncIterator = Reflect::apply(&keys_fn, &self.handle, &Array::new())
-            .map_err(to_directory_error)?
-            .dyn_into()
-            .map_err(|_| TypeError::new(".keys() was not an AsyncIterator"))
-            .map_err(to_directory_error)?;
+        let f = async || -> Result<Vec<Vec<u8>>, JsValue> {
+            let keys_fn: Function =
+                Reflect::get(&self.handle, &JsValue::from("keys"))?.dyn_into()?;
+            let keys_iter: AsyncIterator =
+                Reflect::apply(&keys_fn, &self.handle, &Array::new())?.dyn_into()?;
 
-        let collect_res = collect(&keys_iter);
-        let collected: Array = collect_res
-            .await
-            .map_err(to_directory_error)?
-            .unchecked_into();
-        let mut out = Vec::new();
-        for val in collected {
-            let val = val
-                .as_string()
-                .ok_or_else(|| TypeError::new(".keys() value was not a string"))
-                .map_err(to_directory_error)?
-                .as_bytes()
-                .to_vec();
-            out.push(val);
-        }
-        Ok(out)
+            let collect_res = collect(&keys_iter);
+            let collected: Array = collect_res.await?.dyn_into()?;
+            let mut out = Vec::new();
+            for val in collected {
+                let val = val
+                    .as_string()
+                    .ok_or_else(|| TypeError::new(".keys() value was not a string"))?
+                    .as_bytes()
+                    .to_vec();
+                out.push(val);
+            }
+            Ok(out)
+        };
+        f().await.map_err(to_directory_error)
     }
 
     async fn read_file(&self, name: &[u8]) -> Result<Vec<u8>, DirectoryError> {
-        let handle = self
-            .handle
-            .get_file_handle(str::from_utf8(name).map_err(to_directory_error)?)
-            .await
-            .map_err(to_directory_error)?;
-        let handle: FileSystemFileHandle = handle
-            .dyn_into()
-            .map_err(|_| TypeError::new("FileSystemFileHandle"))
-            .map_err(to_directory_error)?;
-        let file: File = handle
-            .get_file()
-            .await
-            .map_err(to_directory_error)?
-            .dyn_into()
-            .map_err(|_| TypeError::new("File"))
-            .map_err(to_directory_error)?;
-        let data: ArrayBuffer = file
-            .array_buffer()
-            .await
-            .map_err(to_directory_error)?
-            .dyn_into()
-            .map_err(|_| TypeError::new("ArrayBuffer"))
-            .map_err(to_directory_error)?;
-        let data = Uint8Array::new(&data);
-        let mut out = vec![0u8; data.length() as usize];
-        data.copy_to(&mut out);
-        Ok(out)
+        let f = async || -> Result<Vec<u8>, JsValue> {
+            let handle = self
+                .handle
+                .get_file_handle(
+                    str::from_utf8(name).map_err(|_| TypeError::new("name was not UTF-8"))?,
+                )
+                .await?;
+            let handle: FileSystemFileHandle = handle.dyn_into()?;
+            let file: File = handle.get_file().await?.dyn_into()?;
+            let data: ArrayBuffer = file.array_buffer().await?.dyn_into()?;
+            let data = Uint8Array::new(&data);
+            let mut out = vec![0u8; data.length() as usize];
+            data.copy_to(&mut out);
+            Ok(out)
+        };
+        f().await.map_err(to_directory_error)
     }
 }
