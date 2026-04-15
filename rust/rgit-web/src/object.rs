972e5a305cb8d0a683d6e289b0c07ca3a328e7de
cyberia <cyberia@donk.systems>
2026-04-15 18:20:37 +0100

    add WebTree

--- rust/rgit-web/src/object.rs
+++ rust/rgit-web/src/object.rs
@@ -1,8 +1,8 @@
-use rgit_core::object::{Commit, Object};
+use rgit_core::object::{Commit, Object, Tree};
 use serde_wasm_bindgen::to_value;
 use wasm_bindgen::prelude::*;
 
-use crate::directory::WebDirectory;
+use crate::{directory::WebDirectory, error::to_js_error};
 
 #[wasm_bindgen]
 pub struct WebObject(pub(crate) Object<WebDirectory>);
@@ -22,4 +22,11 @@ impl WebCommit {
     pub fn to_js(&self) -> Result<JsValue, JsValue> {
         Ok(to_value(&self.0)?)
     }
+
+    pub async fn tree(&self) -> Result<WebTree, JsValue> {
+        Ok(WebTree(self.0.lookup_tree().await.map_err(to_js_error)?))
+    }
 }
+
+#[wasm_bindgen]
+pub struct WebTree(pub(crate) Tree<WebDirectory>);
