c29cb8a4e8415101c50f6dac95a04c65dd5d8a2f
cyberia <cyberia@donk.systems>
2026-04-25 00:24:25 +0100

    redo diff objects

--- rust/rgit-web/src/diff.rs
+++ rust/rgit-web/src/diff.rs
@@ -1,17 +1,78 @@
-use rgit_core::diff::{DiffEntry, TreeDiff};
-use serde::Serialize;
-use similar::{ChangeTag, TextDiffConfig};
+use crate::{
+    error::to_js_error,
+    js_interop::MaybeUtf8,
+    object::{Tree, TreeEntryType},
+    repo::Repo,
+};
+use rgit_core::diff as rgit_diff;
+use similar::TextDiffConfig;
 use wasm_bindgen::prelude::*;
 
-use crate::{error::to_js_error, object::Tree, repo::Repo};
+#[wasm_bindgen]
+pub enum DiffEntryType {
+    LeftOnly = "left only",
+    Both = "both",
+    RightOnly = "right only",
+}
 
 #[wasm_bindgen]
-pub struct WebDiff(pub(crate) Vec<DiffEntry<Vec<Hunk>>>);
+#[derive(Clone)]
+pub struct DiffEntry(rgit_diff::DiffEntry<Vec<Hunk>>);
 
 #[wasm_bindgen]
-impl WebDiff {
-    pub async fn diff(repo: &Repo, left: &Tree, right: &Tree) -> Result<WebDiff, JsValue> {
-        let tree_diff = TreeDiff::new(&repo.0, &left.0, &right.0)
+impl DiffEntry {
+    pub fn discriminator(&self) -> DiffEntryType {
+        use DiffEntryType::*;
+        match self.0 {
+            rgit_diff::DiffEntry::LeftOnly { .. } => LeftOnly,
+            rgit_diff::DiffEntry::Both { .. } => Both,
+            rgit_diff::DiffEntry::RightOnly { .. } => RightOnly,
+        }
+    }
+
+    #[wasm_bindgen(unchecked_return_type = "MaybeUtf8")]
+    pub fn path(&self) -> JsValue {
+        self.0.path().as_slice().maybe_utf8()
+    }
+
+    pub fn hunks(&self) -> Vec<Hunk> {
+        self.0.content().clone()
+    }
+
+    pub fn left_entry_type(&self) -> TreeEntryType {
+        match self.0 {
+            rgit_diff::DiffEntry::LeftOnly { entry_type, .. }
+            | rgit_diff::DiffEntry::Both {
+                left_type: entry_type,
+                ..
+            } => entry_type.into(),
+            rgit_diff::DiffEntry::RightOnly { .. } => {
+                panic!("no left entry type on right only diff entry")
+            }
+        }
+    }
+
+    pub fn right_entry_type(&self) -> TreeEntryType {
+        match self.0 {
+            rgit_diff::DiffEntry::RightOnly { entry_type, .. }
+            | rgit_diff::DiffEntry::Both {
+                left_type: entry_type,
+                ..
+            } => entry_type.into(),
+            rgit_diff::DiffEntry::LeftOnly { .. } => {
+                panic!("no left entry type on left only diff entry")
+            }
+        }
+    }
+}
+
+#[wasm_bindgen]
+pub struct Diff(pub(crate) Vec<DiffEntry>);
+
+#[wasm_bindgen]
+impl Diff {
+    pub async fn diff(repo: &Repo, left: &Tree, right: &Tree) -> Result<Diff, JsValue> {
+        let tree_diff = rgit_diff::TreeDiff::new(&repo.0, &left.0, &right.0)
             .await
             .map_err(to_js_error)?;
         let diff = tree_diff
@@ -31,11 +92,8 @@ impl WebDiff {
                             .flat_map(|op| text_diff.iter_changes(op))
                             .map(|change| -> Result<_, JsError> {
                                 Ok(LineChange {
-                                    tag: change.tag(),
-                                    value: change
-                                        .as_str()
-                                        .ok_or_else(|| JsError::new("invalid UTF-8"))?
-                                        .to_string(),
+                                    tag: change.tag().into(),
+                                    value: change.value().to_vec(),
                                 })
                             })
                             .collect::<Result<_, _>>()?;
@@ -53,28 +111,63 @@ impl WebDiff {
                     Ok(hunks)
                 })
                 .map_err(|_| JsError::new("invalid UTF-8"))?;
-            entries.push(entry);
+            entries.push(DiffEntry(entry));
         }
-        Ok(WebDiff(entries))
+        Ok(Diff(entries))
     }
 
-    pub fn to_js(&self) -> Result<JsValue, JsValue> {
-        todo!()
+    pub fn entries(&self) -> Vec<DiffEntry> {
+        self.0.clone()
     }
 }
 
-#[derive(Serialize)]
+#[wasm_bindgen]
+#[derive(Clone, Copy)]
+pub enum ChangeTag {
+    Equal = "equal",
+    Delete = "delete",
+    Insert = "insert",
+}
+
+impl From<similar::ChangeTag> for ChangeTag {
+    fn from(value: similar::ChangeTag) -> Self {
+        use ChangeTag::*;
+        match value {
+            similar::ChangeTag::Equal => Equal,
+            similar::ChangeTag::Delete => Delete,
+            similar::ChangeTag::Insert => Insert,
+        }
+    }
+}
+
+#[wasm_bindgen]
+#[derive(Clone)]
 pub struct LineChange {
-    tag: ChangeTag,
-    // value: Vec<u8>,
-    value: String,
+    pub tag: ChangeTag,
+    value: Vec<u8>,
 }
 
-#[derive(Serialize)]
+#[wasm_bindgen]
+impl LineChange {
+    #[wasm_bindgen(unchecked_return_type = "MaybeUtf8")]
+    pub fn value(&self) -> JsValue {
+        self.value.as_slice().maybe_utf8()
+    }
+}
+
+#[wasm_bindgen]
+#[derive(Clone)]
 pub struct Hunk {
-    old_start: usize,
-    old_end: usize,
-    new_start: usize,
-    new_end: usize,
+    pub old_start: usize,
+    pub old_end: usize,
+    pub new_start: usize,
+    pub new_end: usize,
     changes: Vec<LineChange>,
+}
+
+#[wasm_bindgen]
+impl Hunk {
+    pub fn changes(&self) -> Vec<LineChange> {
+        self.changes.clone()
+    }
 }
