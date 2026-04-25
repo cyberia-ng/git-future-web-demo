6c1e03e0215d396ee53371fee7d6d3269ed11b28
cyberia <cyberia@donk.systems>
2026-04-25 14:43:17 +0100

    improve state <-> hash fragment encoding

--- js/src/state.ts
+++ js/src/state.ts
@@ -99,13 +99,84 @@ export function dismissError(idx: number): Mutator<ErrorState> {
 }
 
 export function toUrl(state: AppState): string {
-  if (state.type === "initial") return "";
-  return btoa(JSON.stringify(state));
+  const components: string[] = [];
+  switch (state.type) {
+    case "initial":
+      break;
+    case "file browser": {
+      components.push("browse");
+      switch (state.commit.type) {
+        case "ref": {
+          components.push("ref");
+          switch (state.commit.ref.type) {
+            case "head": {
+              components.push("");
+              break;
+            }
+            case "ref": {
+              components.push(state.commit.ref.name);
+              break;
+            }
+          }
+          break;
+        }
+        case "detached": {
+          components.push("detached");
+          components.push(state.commit.id);
+          break;
+        }
+      }
+      components.push(...state.path);
+      break;
+    }
+    case "commit view": {
+      components.push("commit");
+      components.push(state.commitId);
+      break;
+    }
+  }
+  return components.map(encodeURIComponent).join("/");
 }
 
 export function fromUrl(url: string): AppState {
-  if (url === "") {
-    return initialAppState;
+  const components = url.split("/").map(decodeURIComponent);
+  switch (components.shift()) {
+    case "browse": {
+      const commitComponent = components.shift();
+      let commit: FileBrowserCommit;
+      switch (commitComponent) {
+        case "ref": {
+          const refComponent = components.shift();
+          let ref: RefNamePlainObject;
+          if (refComponent === "" || refComponent === undefined) {
+            ref = { type: "head" };
+          } else {
+            ref = { type: "ref", name: refComponent };
+          }
+          commit = { type: "ref", ref };
+          break;
+        }
+        case "detached": {
+          commit = { type: "detached", id: components.shift() ?? "" };
+          break;
+        }
+        default: {
+          commit = { type: "ref", ref: { type: "head" } };
+          break;
+        }
+      }
+      const path = components;
+      return {
+        type: "file browser",
+        commit,
+        path,
+      };
+    }
+    case "commit": {
+      return { type: "commit view", commitId: components.shift() ?? "" };
+    }
+    default: {
+      return initialAppState;
+    }
   }
-  return JSON.parse(atob(url));
 }
